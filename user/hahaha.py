971dc971ca75de567451c39383b9c76c9e2a97c4
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-20 10:55:06 +1100

    APPELMAN, Max: 2018-02-20 10:55:06

---
+++ user/hahaha.py
@@ -0,0 +1,19 @@
+q=input('ask a question ')
+import random
+r = random.randint(0,6)
+if r==1:
+    print('lol')
+elif r==2:
+    print('elephants will attack tonight')
+elif r==3:
+    print('ha ha no.')
+elif r==4:
+    print('YES OMG')
+elif q==('no'):
+    print('defy me? I will slay you. I have minions all over the universe. you WILL regret this day.')
+else:
+    print('I have no idea what the answer to your question is.')
+
+
+
+    
