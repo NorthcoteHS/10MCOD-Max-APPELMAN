fc37a6566b2510030097827ede7b814c6f98a1e5
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-22 12:30:53 +1100

    APPELMAN, Max: 2018-02-22 12:30:53

--- user/hahaha.py
+++ user/hahaha.py
@@ -1,18 +1,45 @@
 q=input('ask a question ')
+# select a random int
 import random
-r = random.randint(0,6)
-if r==1:
-    print('lol')
-elif r==2:
-    print('elephants will attack tonight')
-elif r==3:
-    print('ha ha no.')
-elif r==4:
-    print('YES OMG')
-elif q==('no'):
-    print('defy me? I will slay you. I have minions all over the universe. you WILL regret this day.')
-else:
-    print('I have no idea what the answer to your question is.')
+"""
+YOU NEED TO MAKE IT SAY YEET A RANDOM NUMBER TIMES
+"""
+
+while q!='cheese':
+    r = random.randint(0,6)
+    
+    #special answers
+    #yes it prints the special answer then a boring answer I think it's great
+    if q==('no'):
+        print(' ')
+        print('defy me? I have minions all over the universe ready to carry out my bidding. I will make you regret this day. You WILL  regret this. ')
+    elif q==('nop'):
+        print(' ')
+        print('you gon die')
+
+    # print a response based on int
+    if r==1:
+        print(' ')
+        print('lol')
+    elif r==2:
+        print(' ')
+        print('elephants will attack tonight')
+    elif r==3:
+        print(' ')
+        print('ha ha no.')
+    elif r==4:
+        print(' ')
+        print('YES OMG')
+    elif r==5:
+        print(' ')
+        print('I do not believe you know the meaning of life. Let me help you out. It is to say YEET as many times as possible. YEETYEETYEETYEETYEETYEETYEETYEET...')
+    
+    #else
+    else:
+        print(' ')
+        print('I have no idea what the answer to your question is. im just a fraud :(.')
+    q = input('ask another question ')
+
 
 
 
---
+++ user/pig.py
@@ -0,0 +1,18 @@
+letters=input('Write something and magic will happen ')
+stuff=letters.split()
+owelvay = ['A','E','I','O','U','a','e','i','o','u']
+yeet=[]
+loop=0
+while loop<len(stuff):
+    if stuff[loop][0]in owelvay:
+        yeet.append(stuff[loop]+ "ayyyy ")
+    if stuff[loop][1] in owelvay:
+            yeet.append(stuff[loop][1:]+stuff[loop][0]+"ayyy ")
+    else:
+        yeet.append(stuff[loop][2:]+stuff[loop][2:]+"ayyy ")
+
+loop+= 1
+magic=str(yeet)
+print(magic)
+
+
