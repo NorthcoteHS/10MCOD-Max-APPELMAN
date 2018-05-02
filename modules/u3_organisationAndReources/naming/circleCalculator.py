dd01a484e0d5a8bb47e662e38a764267424ddbe2
app0001 <app0001@nhs.vic.edu.au>
2018-05-02 14:42:55 +1000

    circle

--- modules/u3_organisationAndReources/naming/circleCalculator.py
+++ modules/u3_organisationAndReources/naming/circleCalculator.py
@@ -1,10 +1,20 @@
+"""
+prog: circleCalculator.py
+name: Max Appelman
+date: 2018/03/12
+desc: calculates area and perimter of a circle.
+"""
+#displays welcome message
 print('Welcome to the Circle Calculator!')
 
+#defines a variable and converts to integer
 r = input('Enter a radius: ')
 r = int(r)
 
+#calculates and prints area of a circle
 area = 3.14 * r * r
 print('The area is', area)
 
+#calculates and prints perimeter of a circle
 perimeter = 3.14 * r * 2
 print('The perimeter is', perimeter)
