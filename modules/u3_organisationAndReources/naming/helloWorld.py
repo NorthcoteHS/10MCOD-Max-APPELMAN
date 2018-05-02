feccc987b8d0c7d8ae3309c177e2bf0584cdcc99
app0001 <app0001@nhs.vic.edu.au>
2018-05-02 14:48:21 +1000

    stuff

--- modules/u3_organisationAndReources/naming/helloWorld.py
+++ modules/u3_organisationAndReources/naming/helloWorld.py
@@ -1,8 +1,7 @@
 """
-Prog:   ???
-Name:   Student Name
-Date:   12/03/18
-Desc:   My first program!
+Prog:   helloWorld.py
+Name:   Max Appelman
+Date:   2018/03/12
+Desc:   Displays a greeting/stupid message
 """
-
 print('Hello, world!')
--- modules/u3_organisationAndReources/naming/rectangleCalculator.py
+++ modules/u3_organisationAndReources/naming/rectangleCalculator.py
@@ -1,6 +1,6 @@
 """
 Prog:   rectCalc.py
-Name:   Student Name
+Name:   Max Appelman
 Date:   12/03/18
 Desc:   Calculates the area and perimeter of a rectangle.
 """
@@ -10,19 +10,19 @@ print('Welcome to the Rectangle Calculator!')
 
 # Use input to get the rectangle's length and width (2 lines).
 # - Remember to provide a prompt message for each input.
-
-
+length=input("Enter the length: ")
+width=input("Enter the width: ")
 
 # Convert length and width to integers.
 length = int(length)
 width = int(width)
 
 # Calculate the area (1 line: length times width).
-
+area = length*width
 
 # Display the area.
 print('The area is', area)
 
 # Calculate and display the perimeter (2 lines: P = 2*length + 2*width).
-
-
+perimeter = 2*length + 2*width
+print('the perimter is', perimeter)
