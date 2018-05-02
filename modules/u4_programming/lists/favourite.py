a2f10a46564c63b9b50c60dd1d8b80f984c8a09b
app0001 <app0001@nhs.vic.edu.au>
2018-05-02 14:17:11 +1000

    because

--- modules/u4_programming/lists/favourite.py
+++ modules/u4_programming/lists/favourite.py
@@ -2,6 +2,6 @@ favourites=[]
 no=input("What is one of ur favourite movies? ")
 while no!='quit':
     yes=input('rate out of 5 ')
-    favourites.append([no,yes])
+    favourites.append([no])
     no=input("What is one of ur favourite movies? ")
 print("Thanks boi,you stored", len(favourites),"movies")
