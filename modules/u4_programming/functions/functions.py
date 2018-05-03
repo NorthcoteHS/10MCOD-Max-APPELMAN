eb5bba6133e9eca10538b085252cd83957a53c21
app0001 <app0001@nhs.vic.edu.au>
2018-05-03 12:31:39 +1000

    magic

---
+++ modules/u4_programming/functions/functions.py
@@ -0,0 +1,26 @@
+
+
+def hooray():
+    print("hooray")
+
+def subtract(x,y):
+    return x - y
+
+def double(x):
+    return x*2
+
+def excited(word):
+    return word+'!'
+
+def stringJoin(str1, str2):
+    return str1+' '+ str2
+
+def birthday(name):
+    print ('Happy birthday, '+ name +'!')
+
+hooray()
+print(subtract(2,1))
+print(double(5))
+print(excited('lol'))
+print(stringJoin('lol','lolo'))
+birthday('diii')
