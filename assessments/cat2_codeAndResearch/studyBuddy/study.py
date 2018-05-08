e6ead9faa64894a83c12e6c93e0c59b316be79e3
app0001 <app0001@nhs.vic.edu.au>
2018-05-08 10:53:16 +1000

    ye

--- assessments/cat2_codeAndResearch/studyBuddy/study.py
+++ assessments/cat2_codeAndResearch/studyBuddy/study.py
@@ -2,7 +2,7 @@
 prog: study.py
 name: Max A
 date: 2018/05/08
-desc:
+desc: asks user a series of questions and gives feedback after each question and at the end
 """
 
-questions=['Which of these is not a breed of dog?', 'What is the melting point of water?', 'Best way to disarm a knife in close range(<2m)?', 'Which of these is not a vegetable?', 'How do you make bronze?', 'If price increases...', 'What is 12x12?']
+questions=['Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois', 'What is the melting point of water? A) 100', 'Best way to disarm a knife in close range(<2m)?', 'Which of these is not a vegetable?', 'How do you make bronze?', 'If price increases...', 'What is 12x12?']
