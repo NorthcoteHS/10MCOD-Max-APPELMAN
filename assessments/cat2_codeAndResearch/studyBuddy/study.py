697cf17d5f275f915bbccdd24a726016b5d148d0
app0001 <app0001@nhs.vic.edu.au>
2018-05-08 10:44:37 +1000

    question

--- assessments/cat2_codeAndResearch/studyBuddy/study.py
+++ assessments/cat2_codeAndResearch/studyBuddy/study.py
@@ -5,4 +5,4 @@ date: 2018/05/08
 desc:
 """
 
-questions=['']
+questions=['Which of these is not a breed of dog?', 'What is the melting point of water?', 'Best way to disarm a knife in close range(<2m)?', 'Which of these is not a vegetable?', 'How do you make bronze?', 'If price increases...', 'What is 12x12?'
