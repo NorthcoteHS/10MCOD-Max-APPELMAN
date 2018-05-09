d147f272777c1a4fcaec6dcd1d294b9bc895212b
app0001 <app0001@nhs.vic.edu.au>
2018-05-09 13:45:24 +1000

    things

--- assessments/cat2_codeAndResearch/studyBuddy/study.py
+++ assessments/cat2_codeAndResearch/studyBuddy/study.py
@@ -2,7 +2,16 @@
 prog: study.py
 name: Max A
 date: 2018/05/08
-desc: asks user a series of questions and gives feedback after each question and at the end
+desc: asks user a series of questions, gives correct/incorrect feedback, and tallies score to display at the end
 """
 
-questions=['Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois', 'What is the melting point of water? A) 100', 'Best way to disarm a knife in close range(<2m)?', 'Which of these is not a vegetable?', 'How do you make bronze?', 'If price increases...', 'What is 12x12?']
+
+questions=['Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois', 'What is the melting point of water? A) 100 celsius B) 100 Fahrenheit C) 212 Fahrenheit D) 32 Fahrenheit', 'Best way to disarm a knife in close range(<2m)? A) Run away B) get stabbed then play dead C) get stabbed then be dead D) grab the knife, punch assailant and run away', 'Which of these is a berry? A) celery B) strawberry C) banana D) celeriac', 'How do you make bronze? A) tin and copper B) gold minus silver C) gold plus silver D) iron and gold', 'If price increases... A) demand expands and supply expands B) deamnd contracts and supply contracts C) demand expands and supply contracts D) demand contracts and supply expands', 'What is 12x12? A) 144 B) 48 C) 24 D) 121']
+
+correctAnswers=['B','D','D','C','A','D','A']
+
+def q:
+    for n in questions:
+    answer=input(n)
+    if answer==
+
