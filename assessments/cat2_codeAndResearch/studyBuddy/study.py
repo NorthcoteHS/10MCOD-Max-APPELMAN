d36a56915b439f89d8a8ea66085ab106a94b1ea3
app0001 <app0001@nhs.vic.edu.au>
2018-05-10 11:58:49 +1000

    lol

--- assessments/cat2_codeAndResearch/studyBuddy/study.py
+++ assessments/cat2_codeAndResearch/studyBuddy/study.py
@@ -6,10 +6,10 @@ desc: asks user a series of questions, gives correct/incorrect feedback, and tal
 """
 
 #displays welcome message
-"HELLO THIS IS A QUIZ 4 SKILLED PEOPLE PLZ ANSWER A B C OR D THNKS"
+print("HELLO THIS IS A QUIZ 4 SKILLED PEOPLE PLZ ANSWER A B C OR D THNKS")
 
 #list of questions and multiple choice answers
-questions=['1. Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois. answer= ', '2. What is the melting point of ice (not the drug)? A) 100 celsius B) 100 Fahrenheit C) 212 Fahrenheit D) 32 Fahrenheit. answer= ', '3. What is the best/safest way to disarm a knife in close range(<2m)? A) Run away B) get stabbed then play dead C) get stabbed then be dead D) grab the knife, punch assailant and run away. answer= ', '4. Which of these is a berry? A) celery B) strawberry C) banana D) celeriac. answer= ', '5. How do you make bronze? A) tin and copper B) gold minus silver C) gold plus silver D) iron and gold. answer= ', '6. If price increases... A) demand expands and supply expands B) demand contracts and supply contracts C) demand expands and supply contracts D) demand contracts and supply expands. answer= ', '7. What is 12x12? A) 144 B) 48 C) 24 D) 121. answer= ', '8. What is the capital of Turkey? A) Bangladesh B)Ankara C)Istanbul D) Rabat', '9. Who is the founder of Amazon? A) Steve Jobs B) Bill Gates C) Jeff Bezos D) Hilary Clinton', '10. Which of these cells have cell walls? A) plant cells B) plant and animal cells C) animal cells D) bacteria and protozoa cells']
+questions=['1. Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois. answer= ', '2. What is the melting point of ice (not the drug)? A) 100 celsius B) 100 Fahrenheit C) 212 Fahrenheit D) 32 Fahrenheit. answer= ', '3. What is the best/safest way to disarm a knife in close range(<2m)? A) Run away B) get stabbed then play dead C) get stabbed then be dead D) grab the knife, punch assailant and run away. answer= ', '4. Which of these is a berry? A) celery B) strawberry C) banana D) celeriac. answer= ', '5. How do you make bronze? A) tin and copper B) gold minus silver C) gold plus silver D) iron and gold. answer= ', '6. If price increases... A) demand expands and supply expands B) demand contracts and supply contracts C) demand expands and supply contracts D) demand contracts and supply expands. answer= ', '7. What is 12x12? A) 144 B) 48 C) 24 D) 121. answer= ', '8. What is the capital of Turkey? A) Bangladesh B)Ankara C)Istanbul D) Rabat', '9. Who is the founder of Amazon? A) Steve Jobs B) Bill Gates C) Jeff Bezos D) Hilary Clinton', '10. Which of these cells have cell walls? A) plant cells B) plant and animal cells C) animal cells D) bacteria and protozoa cells. answer= ']
 
 #list of correct answers
 correctAnswers=['B','D','D','C','A','D','A','B','C','A']
@@ -45,6 +45,14 @@ elif score==4 or score==5 or score==6:
 else:
     print('(Wow you really suck nah its all g)')
 
+
+x=input('Please rate how much you like this quiz out of 5!')
+if x==5:
+    print("YEE THANKS!")
+elif x==3 or x=4:
+    print("Well that's a bit rude")
+else:
+    print('YOUR SO MEANNN')
 
 
 
