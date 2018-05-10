09aaa03653cf6cf344eb3ad29df3f4cc6593ae81
app0001 <app0001@nhs.vic.edu.au>
2018-05-10 11:51:27 +1000

    now its 10

--- assessments/cat2_codeAndResearch/studyBuddy/study.py
+++ assessments/cat2_codeAndResearch/studyBuddy/study.py
@@ -9,10 +9,10 @@ desc: asks user a series of questions, gives correct/incorrect feedback, and tal
 "HELLO THIS IS A QUIZ 4 SKILLED PEOPLE PLZ ANSWER A B C OR D THNKS"
 
 #list of questions and multiple choice answers
-questions=['Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois. answer= ', 'What is the melting point of ice (not the drug)? A) 100 celsius B) 100 Fahrenheit C) 212 Fahrenheit D) 32 Fahrenheit. answer= ', 'Best way to disarm a knife in close range(<2m)? A) Run away B) get stabbed then play dead C) get stabbed then be dead D) grab the knife, punch assailant and run away. answer= ', 'Which of these is a berry? A) celery B) strawberry C) banana D) celeriac. answer= ', 'How do you make bronze? A) tin and copper B) gold minus silver C) gold plus silver D) iron and gold. answer= ', 'If price increases... A) demand expands and supply expands B) demand contracts and supply contracts C) demand expands and supply contracts D) demand contracts and supply expands. answer= ', 'What is 12x12? A) 144 B) 48 C) 24 D) 121. answer= ']
+questions=['1. Which of these is not a breed of dog? A) Pugador B) Prince Charles Spaniel C) Beaglier D) Griffon Bruxellois. answer= ', '2. What is the melting point of ice (not the drug)? A) 100 celsius B) 100 Fahrenheit C) 212 Fahrenheit D) 32 Fahrenheit. answer= ', '3. What is the best/safest way to disarm a knife in close range(<2m)? A) Run away B) get stabbed then play dead C) get stabbed then be dead D) grab the knife, punch assailant and run away. answer= ', '4. Which of these is a berry? A) celery B) strawberry C) banana D) celeriac. answer= ', '5. How do you make bronze? A) tin and copper B) gold minus silver C) gold plus silver D) iron and gold. answer= ', '6. If price increases... A) demand expands and supply expands B) demand contracts and supply contracts C) demand expands and supply contracts D) demand contracts and supply expands. answer= ', '7. What is 12x12? A) 144 B) 48 C) 24 D) 121. answer= ', '8. What is the capital of Turkey? A) Bangladesh B)Ankara C)Istanbul D) Rabat', '9. Who is the founder of Amazon? A) Steve Jobs B) Bill Gates C) Jeff Bezos D) Hilary Clinton', '10. Which of these cells have cell walls? A) plant cells B) plant and animal cells C) animal cells D) bacteria and protozoa cells']
 
 #list of correct answers
-correctAnswers=['B','D','D','C','A','D','A']
+correctAnswers=['B','D','D','C','A','D','A','B','C','A']
 
 #sets starting score
 score = 0
